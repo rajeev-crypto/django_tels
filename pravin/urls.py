4ee22c6a27c26e78ae2b8f7d7c90c0a276101d7d
Rajeev <rajeev@gmail.com>
2021-02-07 11:17:44 +0530

    added two number program

--- pravin/urls.py
+++ pravin/urls.py
@@ -2,5 +2,6 @@ from  django.urls import path
 from . import views
 
 urlpatterns = [
-  path('',views.priyank,name='pryank')
+  path('',views.priyank,name='pryank'),
+  path('add',views.add,name='add')
 ]
--- pravin/views.py
+++ pravin/views.py
@@ -5,4 +5,12 @@ from django.http import HttpResponse
 def priyank(request):
     #return HttpResponse("Hello World i a mdjango ")
     return render(request,'index.html',{'name':'Navin'})
+
+def add(request):
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"])
+    res = val1+ val2
+    return render(request,"result.html",{'result':res})
+
+
 # Create your views here.
