99b6bc7eca32539b0e026a868cd15470e908cb25
Rajeev <rajeev@gmail.com>
2021-03-29 04:08:16 +0530

    added chanegs in making dynamic pages

--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Destination:
+    id: int
+    name: str
+    img: str
+    des: str 
 # Create your models here.
--- travel/views.py
+++ travel/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def index(request):
-	return render(request,"index.html")
+
+	dest1 = Destination()
+	dest1.name = 'FIRST PERSON'
+	dest1.desc = 'efbwfobwoif aofiwe adficnf dd'
+	dest1.img = 'air-balloon.png'
+
+	dest2 = Destination()
+	dest2.name = 'SECOND PERSON'
+	dest2.desc = 'efbwfobwoif aofiwe adficnf ddf'
+	dest2.img = 'air-balloon.png'
+
+	dest3 = Destination()
+	dest3.name = 'THIRD PERSON'
+	dest3.desc = 'efbwfobwoif aofiwe adficnf ddf'
+	dest3.img = 'air-balloon.png'
+
+	dests = [dest1,dest2,dest3]
+	return render(request,"index.html",{"dests": dests })
